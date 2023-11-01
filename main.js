4bcbd5cebcc619fd1156f07c2f5cd3af28267830
TYP3D <114131693+TYP3D@users.noreply.github.com>
2023-11-01 20:53:31 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,1 @@
-document.getElementById("passkeyForm").addEventListener("submit", function (event) {
-    event.preventDefault();
-    const passkey = document.getElementById("passkeyInput").value;
 
-    // Replace 'your_passkey' with the actual passkey you want to use
-    if (passkey === "your_passkey") {
-        window.location.href = "your-protected-page.html";
-    } else {
-        document.getElementById("message").textContent = "Invalid passkey. Please try again.";
-    }
-});
